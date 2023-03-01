95c2d367c4010a217e64a36a520775c7659c8ec5
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-01 19:04:42 -0300

    Tab bar settings

---
+++ ContactList/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  ContactList
+//
+//  Created by Ilya Zemskov on 01.03.2023.
+//
+
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+    }
+}
